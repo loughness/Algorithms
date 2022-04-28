dd21490066765d325c0af2c9261a4fcba2d84b81
loughness <21imc10058@fh-krems.ac.at>
2022-04-28 19:16:03 +0200

    Taken out reference code

--- Bubble Sort.py
+++ Bubble Sort.py
@@ -1,19 +1,5 @@
 
 to_sort = [13, 2, 63, 4, 18, 45, 37, 9]
-def bubbleSort(list):
-    n = len(list)
-    h = 1
-    for i in range(n-1):
-        flag = 0
-        for j in range(0,n-i-1):
-            if list[j] > list[j+1]:
-                list[j], list[j+1] = list[j+1], list[j]
-            print(f"Round {h} -> {list}")
-            h += 1
-        if flag == 0:
-            break
-    return list
-
 def bubble(list):
     # need to get the length of what we will be comparing
     # the -1 is because we cannot perform a comparison on the last item of the list : index out of range
@@ -24,7 +10,6 @@ def bubble(list):
     h = 1
 
     while not sorted:
-        #
         sorted = True
         for i in range(0,n):
             # after sorting all items, this if statement won't activate
@@ -34,7 +19,6 @@ def bubble(list):
                 list[i], list[i+1] = list[i+1], list[i]
                 print(f"Round {h} -> {list}")
                 h += 1
-
 
 # bubbleSort(to_sort)
 bubble(to_sort)
--- Selection Sort.py
+++ Selection Sort.py
@@ -27,21 +27,3 @@ def selection_sort(list):
 
 # print(selection_sort(to_sort))
 selection_sort(to_sort)
-
-
-def selection_sort2(list_a):
-    indexing_length = range(0, len(list_a)-1)
-
-    for i in indexing_length:
-        min_value = i
-
-        for j in range(i+1, len(list_a)):
-            if list_a[j] < list_a[min_value]:
-                min_value = j
-
-        if min_value != i:
-            list_a[min_value], list_a[i] = list_a[i], list_a[min_value]
-
-    return list_a
-
-print(selection_sort2(to_sort))
